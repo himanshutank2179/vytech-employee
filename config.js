23d2d12b1823c44efd5b66fef3b985c33dd22850
himanshu <himanshutank1111@gmail.com>
2017-10-28 15:58:06 +0530

    config db creadentials updated

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-	"database":"mongodb://himanshu:Suprimesoul2179@ds129053.mlab.com:29053/node_project",
+	//"database":"mongodb://himanshu:Suprimesoul2179@ds129053.mlab.com:29053/node_project",
+	"database":"mongodb://root:Vy@s250994@nodejs.vytech.co:27017/vytech_employee_db",
 	"port":process.env.PORT || 3000,
 	"secretKey":"YourSecretKEY"
 }
